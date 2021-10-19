cecc203124eedeac6d045615e5d0a57a845f91a4
Duvauwn Prendergast <duvauwnprendergast@gmail.com>
2021-10-19 04:19:24 +0100

    fixing table creation

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// My Array of objects
 let myLibrary = [
     {
         title: 'The Hobbit', author: 'J.R.R. Tolkien', pages: '245 pages',
@@ -12,20 +13,24 @@ let myLibrary = [
         read: 'read it',
     }
 ];
-
-function Book(title, author, pages, read) {
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.read = read
-    this.info = function () {
-        return title + ' by ' + author + ', ' + pages + ', ' + read;
+// The Book Object Constructor
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+        //this.info = function () {
+        //    return title + ' by ' + author + ', ' + pages + ', ' + read;
+        //};
     }
 }
+// The Add Book Function
 function addBookToLibrary(title, author, pages, read) {
     const book = new Book(title, author, pages, read);
     myLibrary.push(book);
 }
+// functions for pop-up form
 function openForm() {
     document.getElementById('bookForm').style.display = 'block';
 }
@@ -34,24 +39,32 @@ function closeForm() {
 }
 addBookToLibrary('test', 'me', '23 pages', 'read it');
 
-let books = document.querySelector('#books');
+//Creates a table that displays my array of objects
+let myTable = document.querySelector('#table');
+let headers = ['Title', 'Author', 'Pages', 'Read'];
 
-let html = '<table border="1|1">';
-html += '<thead>';
-html += '<th>Title</th>';
-html += '<th>Author</th>';
-html += '<th>Pages</th>';
-html += '<th>Read</th>';
-html += '</thead>';
-for (let i = 0; i < myLibrary.length; i++) {
-    html += "<tr>";
-    html += "<td>" + myLibrary[i].title + "</td>";
-    html += "<td>" + myLibrary[i].author + "</td>";
-    html += "<td>" + myLibrary[i].pages + "</td>";
-    html += '<td>' + myLibrary[i].read + '</td>';
-    html += '<td>' + '<button>' + 'Remove' + '</button>' + '</td>';
+let table = document.createElement('table');
+let headerRow = document.createElement('tr');
 
-    html += "</tr>";
-}
-html += '</table>';
-document.getElementById('books').innerHTML = html;
+headers.forEach(headerText => {
+    let header = document.createElement('th');
+    let textNode = document.createTextNode(headerText);
+    header.appendChild(textNode);
+    headerRow.appendChild(header);
+})
+table.appendChild(headerRow);
+
+myLibrary.forEach(novel => {
+    let row = document.createElement('tr');
+
+    Object.values(novel).forEach(text => {
+        let cell = document.createElement('td');
+        let textNode = document.createTextNode(text);
+        cell.appendChild(textNode);
+        row.appendChild(cell);
+    })
+
+    table.appendChild(row);
+});
+
+myTable.appendChild(table);
